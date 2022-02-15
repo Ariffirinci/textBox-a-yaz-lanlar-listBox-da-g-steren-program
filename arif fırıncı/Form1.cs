4970088265ac11f8d86c2b3449570f00d138076c
Ariffirinci <99245364+Ariffirinci@users.noreply.github.com>
2022-02-15 11:53:31 +0300

    Add files via upload

---
+++ arif fırıncı/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arif_fırıncı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            int sayı;
+
+            while (i < Convert.ToInt32(textBox2.Text))
+            {
+             //   listBox1.Items.Add("");
+                listBox1.Items.Add(textBox1.Text);
+                sayı = Convert.ToInt32(textBox2.Text);
+                i++;
+            }
+             
+            
+                
+            
+           
+               
+               
+           
+        }
+    }
+}
